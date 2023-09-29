4f79961cda45e94ccf52d616e9b5b54aa2b79d6b
Mo Figueroa <moisesdfigueroa@gmail.com>
2023-09-28 20:58:00 -0400

    Adds env from Az
    Signed-off-by: Mo Figueroa <moisesdfigueroa@gmail.com>

--- chatgpt/settings.py
+++ chatgpt/settings.py
@@ -9,24 +9,27 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)_fe!&8j!sjt%gu)lp48a8*z+#9+f-5u&tr1kbk7_m=uodo434'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
 
-ALLOWED_HOSTS = ['ayytiti.azurewebsites.net','https://ayytiti.azurewebsites.net']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS').split(' ')
 
+SECURE_SSL_REDIRECT = \
+    os.getenv('SECURE_SSL_REDIRECT', '0').lower() in ['true', 't', '1']
+if SECURE_SSL_REDIRECT:
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Application definition
 
@@ -73,8 +76,6 @@ WSGI_APPLICATION = 'chatgpt.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-CSRF_TRUSTED_ORIGINS = ['ayytiti.azurewebsites.net','https://ayytiti.azurewebsites.net']
 
 DATABASES = {
     'default': {
